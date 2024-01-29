b8891c51aeeb404d365dc8ef01926c0943656594
Tolik <Tolik@MAYZERPC>
2024-01-29 21:56:28 +0300

    Добавьте файлы проекта.

---
+++ work/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <math.h>
+
+int main()
+{
+	float x;
+	float y;
+	cout << "Введите чему равен x - " << endl;
+	cin >> x;
+
+	if (x != 0 && x >= -1)
+	{
+		y = (1 / x) + (sqrt(x + 1));
+		cout << "y = " << y << endl;
+	}
+	else
+	{
+		cout << "Функция не определена " << endl;
+	}
+}
